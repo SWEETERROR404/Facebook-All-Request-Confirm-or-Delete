555edc2cde3c7f9751b2be524645faf1420ab593
Mubeen Ahmad <70522579+CodeWithMubeen@users.noreply.github.com>
2020-09-13 16:14:53 +0500

    Update Mubeen.py

--- Mubeen.py
+++ Mubeen.py
@@ -1,4 +1,4 @@
-import sys,os,time,http.cookiejar,urllib.request,getpass,requests,bs4
+import os,time,http.cookiejar,urllib.request,getpass,requests,bs4
 logo = """\033[1;32;40m
         __                                           __
        (**)                                         (**)
@@ -80,13 +80,11 @@ time.sleep(2)
 os.system('clear')
 print(logo)
 print(f'{l1}\n{l2}\n{l3}\n{l4}')
-
 cj = http.cookiejar.CookieJar()
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 urllib.request.install_opener(opener)
 authentication_url = "https://m.facebook.com/login.php"
 payload = {
-
     'email': email,
     'pass': password
 }
@@ -95,30 +93,29 @@ data = urllib.parse.urlencode(payload).encode('utf-8')
 req = urllib.request.Request(authentication_url, data)
 resp = urllib.request.urlopen(req)
 contents = resp.read()
+def searcher(url):
+    data = requests.get(url, cookies=cj)
+    soup = bs4.BeautifulSoup(data.text, "html.parser")
+    return soup
+error = searcher(authentication_url).find(id='login_error')
+if error != None:
+    print(str(error).split("=")[2].split('"')[1].replace("_",' ').casefold())
 print("Please wait for Extracting all Requests")
 #Go to Friends Requests
 url = "https://mbasic.facebook.com/friends/center/requests/#friends_center_main"
-data = requests.get(url, cookies=cj)
-soup = bs4.BeautifulSoup(data.text, "html.parser")
 #see_more links extract
 see_more = ["https://mbasic.facebook.com/friends/center/requests/"]
 for i in see_more:
-    data = requests.get(i, cookies=cj)
-    soup = bs4.BeautifulSoup(data.text, "html.parser")
-    for x in soup.findAll(id="u_0_0"):
+    for x in searcher(i).findAll(id="u_0_0"):
         for link in x.findAll("a"):
             href = link.get("href")
             if href[16] == "r":
-                see_more.append("https://mbasic.facebook.com"+str(href))
-            else:
-                break
+                var = see_more.append("https://mbasic.facebook.com" + str(href)) == True
 friends_name = []
 confirm = []
 delete = []
 # class bm for link 0
-data = requests.get(see_more[0], cookies=cj)
-soup = bs4.BeautifulSoup(data.text, "html.parser")
-for i in soup.findAll('div', class_="bm"):
+for i in searcher(see_more[0]).findAll('div', class_="bm"):
     no = 0
     for a in i.findAll('a'):
         if no == 0:
@@ -126,44 +123,42 @@ for i in soup.findAll('div', class_="bm"):
             friends_name.append(a.text.replace("‏‎", "").replace("‎‏", "").replace("‏", ""))
         elif no == 1:
             # confirm request
-            confirm.append('https://mbasic.facebook.com'+str(a).replace('"', "\n").split("\n")[3].replace("amp;",""))
+            confirm.append('https://mbasic.facebook.com' + str(a).replace('"', "\n").split("\n")[3].replace("amp;", ""))
         elif no == 2:
             # Delete request
-            delete.append('https://mbasic.facebook.com'+str(a).replace('"', "\n").split("\n")[3].replace("amp;",""))
+            delete.append('https://mbasic.facebook.com' + str(a).replace('"', "\n").split("\n")[3].replace("amp;", ""))
         no += 1
-# class bk for link 1 to end
 for next_class in see_more:
-    data = requests.get(next_class, cookies=cj)
-    soup = bs4.BeautifulSoup(data.text, "html.parser")
-    for i in soup.findAll('div', class_="bk"):
+   for i in searcher(next_class).findAll('div', class_="bk"):
         no = 0
         for a in i.findAll('a'):
             if no == 0:
                 friends_name.append(a.text.replace("‏‎","").replace("‎‏","").replace("‏",""))
             elif no == 1:
                 # confirm request
-                confirm.append('https://mbasic.facebook.com'+str(a).replace('"', "\n").split("\n")[3].replace("amp;",""))
+                confirm.append('https://mbasic.facebook.com' + str(a).replace('"', "\n").split("\n")[3].replace("amp;", ""))
             elif no == 2:
                 # Delete request
-                delete.append('https://mbasic.facebook.com'+str(a).replace('"', "\n").split("\n")[3].replace("amp;",""))
+                delete.append('https://mbasic.facebook.com' + str(a).replace('"', "\n").split("\n")[3].replace("amp;", ""))
             no += 1
+
 print("Total Requests "+str(len(friends_name)))
-user_input = int(input("Type 1 for All Accepted\nType 2 for All Deleted\n"))
-#Confirm
-if user_input == 1:
-    for name in friends_name:
-        print(name + "\nConfirm Request\n")
-        print("Please Wait")
-    for con in confirm:
-        data = requests.get(con, cookies=cj)
-        soup = bs4.BeautifulSoup(data.text, "html.parser")
-#Delete
-elif user_input == 2:
-    for name in friends_name:
-        print(name + "\nDelete Request\n")
-        print("Please Wait")
-    for remove in delete:
-        data = requests.get(remove, cookies=cj)
-        soup = bs4.BeautifulSoup(data.text, "html.parser")
-else:
-        print("Wronge Input")
+while True:
+        user_input = input("Type 1 for All Accepted\nType 2 for All Deleted\n")
+        if user_input > "2" or user_input < "1":
+            print("Please Type Only 1 or 2 Number ")
+        else:
+            if int(user_input) == 1:
+                for name in friends_name:
+                    print(str(friends_name) + "\nConfirm Request\n")
+                    print("Please Wait")
+                for con in confirm:
+                    searcher(con)
+                break
+            else:
+                for name in friends_name:
+                    print(str(friends_name) + "\nDelete Request\n")
+                    print("Please Wait")
+                for remove in delete:
+                    searcher(remove)
+                break
